6301eee5622d8ee41929e9350a3a40253c7c2536
MilanoBeer <tgd5753@gmail.com>
2023-01-09 20:27:13 +0900

    [BOJ] 구현 / Bronze

---
+++ 구현/Bronze/BJ_10817_세수.py
@@ -0,0 +1,6 @@
+
+
+num_list = list(map(int, input().split()))
+num_list.sort()
+print(num_list[1])
+
---
+++ 구현/Bronze/BJ_15596_정수N개의합.py
@@ -0,0 +1,6 @@
+def solve(a):
+    sum = 0
+    for i in a:
+        sum += i
+    
+    return sum
