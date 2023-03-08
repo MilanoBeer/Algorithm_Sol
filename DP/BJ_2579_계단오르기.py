be410b2f78a772322824e500e37b06d980dc3146
MilanoBeer <tgd5753@gmail.com>
2023-03-08 16:55:48 +0900

    [BOJ]_2579_계단오르기 / SILVER3 / DP

--- DP/BJ_2579_계단오르기.py
+++ DP/BJ_2579_계단오르기.py
@@ -1,20 +1,28 @@
+# 구하려는 값 ? 
+    # 현재 계단 위치에서(밟거나 or 밟지 않거나 ), 최대가 되는 누적합 
+    # DP 테이블 : 현재 계단위치에서 최대가 되는 누적합값 
+
+# 특정상황에서 선택 가능한 경우는 무엇이 있나? 
+    # 현재 계단위치를 밟거나 밝지 않거나 
+    # 단, 밝을 수 없을 경우는 고려대상에서 제외 
+
+N = int(input()) # 계단의 갯수 
+DP = [0] * (N)
+n_l = []
+
 # input data 
-n = int(input()) # 계단의 갯수 
-stair_list = []
-for i in range(n):
-    stair_list.append(int(input()))
+for i in range(N):
+    n_l.append(int(input()))
 
-D = [[0] * n for _ in range(4)] # 4행 n열
+if len(n_l) <= 2:
+    print(sum(n_l))
+else:
+    # 도출할 수 있는 현재 값
+    DP[0] = n_l[0]
+    DP[1] = n_l[0] + n_l[1]
+    # DP[2] = max(DP[0] + n_l[2], n_l[1] + n_l[2], DP[0] + n_l[1])
+    DP[2] = max(DP[0] + n_l[2], n_l[1] + n_l[2])
+    for i in range(3, N):
+        DP[i] = max(DP[i-2] + n_l[i], DP[i-3] + n_l[i-1] + n_l[i])
 
-D[0][0] = stair_list[0]
-D[1][0] = stair_list[0]
-D[2][0] = 0
-D[3][0] = 0
-
-for i in range(1, n):
-    D[0][i] = D[1][i-1] + stair_list[i]
-    D[1][i] = max(D[2][i-1] + stair_list[i], D[3][i-1] + stair_list[i])
-    D[2][i] = D[0][i-1]
-    D[3][i] = D[1][i-1]
-
-print(max(D[0][n-1], D[1][n-1]))
+    print(DP[-1])
