e0c57918209274d4a4bb2095dcac2478d1378545
MilanoBeer <86315623+MilanoBeer@users.noreply.github.com>
2023-05-01 20:10:01 +0900

    [Silver V] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 320 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
@@ -1,11 +1,13 @@
- # 1초 / 128MB 
-# 7: 20 ~ 
-# N종류의 아이스크림 / 1번 ~ 
+# 1초 / 128MB 
+# 19: 20 ~ 20:05
+# N종류의 아이스크림 / 1번 ~ N번
 
-# 특정 조합을 피해서 먹으려고 함
-# 3가지 선택
+# 특정 조합을 피해서 먹으려고 함 # 3가지 선택
+
+import sys
 from itertools import combinations
 
+input = sys.stdin.readline
 # N종류 : 1이상, 200이하 / M : 먹으면 안되는 조합의 갯수 : 0이상, 10,000이하 
 N, M = map(int, input().split())
 
@@ -17,19 +19,21 @@ for m in range(M):
     visited[u][v] = 1
 
 # Solution 
-    # -> dic활용
 glo_ans_cnt = 0
 # 선택하고 나서 -> M개의 리스트에 있는지 확인하자
 def check(case):
+    """
+    case의 3가지 값을 뽑아서, visited배열로 특정 조합에 해당하는지 확인하기 
+    """
     global glo_ans_cnt
     a, b, c = case
-    # case의 각 아이스크림 번호에 대해
+
+    # case의 각 아이스크림 번호에 대해 : 모든 경우를 피하면
     if visited[a][b] == 0 and visited[a][c] == 0 and visited[b][c] == 0:
         glo_ans_cnt += 1
             
 ice = list(range(1, N+1))
 for case in combinations(ice, 3):
-    # IDEA : N에서 3개 뽑기 -> 200_C_3 -> 200.199.198 // 6 -> 8백만
     check(case)
 
 # Output > 선택할 수 있는 방법의 수 
