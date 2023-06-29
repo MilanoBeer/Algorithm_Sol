1b8587dd9e73bef28299087b6848ec082471af79
Hyeyeon Shin <86315623+MilanoBeer@users.noreply.github.com>
2023-06-29 10:38:36 +0900

    [Silver II] Title: 한 줄로 서기, Time: 2776 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -1,6 +1,6 @@
 # 2초 / 128MB
 # 23.06.29
-# 09:27 ~
+# 09:27 ~ 10:35
 
 # 자기보다 큰 사람이 왼쪽에 몇 명 있었는지 
 # 1 ~ N
@@ -12,22 +12,14 @@ def input():
 N = int(input()) # 1이상, 10이하 *** 
 _data = list(map(int, input().split()))
 
-# 1. 완탐? 
-# 2. DP 
-# 3. 그리디
-# 4. 스택, 큐
-# 5. 정렬 알고리즘
-
+# 1. 완탐?  # 2. DP  # 3. 그리디 # 4. 스택, 큐 # 5. 정렬 알고리즘
 _ans = [0] * (N+1)
 
 def check(idx, n, strd):
-    # print(*_ans[:-1])
-    # print("check : ", idx, n, strd)
     cnt = 0
     for i in range(idx):
         if _ans[i] > n:
             cnt += 1
-    # print("check : ", idx, n, strd, "cnt :", cnt)
     if strd == cnt:
         return True
     else:
@@ -35,29 +27,23 @@ def check(idx, n, strd):
 
 def dfs(n):
     if n == N:
-        # 이때 정상적인지 확인! 채우는 도중이 아니라!
+        # ERROR : 배열 다 만들고! 정상적인지 확인! 채우는 도중이 아니라! ***** 
         isOrder = True
         for i in range(N):
-            # 6 2 3 4 7 5 1 
+            # 6 2 3 4 7 5 1  # 0 ~ i-1까지 / n+1 보다 큰 숫자들 카운트 
+            # PARAM : 0, 6, data정보의 6 - 1번째 값 (= 왼편 더 큰 거 몇개인가)
             if not check(i, _ans[i], _data[_ans[i] - 1]):
                 isOrder = False
                 break 
         if isOrder:
             print(*_ans[:-1])
-
         return 
     
-    tmp = _data[n]
-    # print("n :", n, ", tmp:", tmp)
-
-    for i in range(tmp, N):
+    for i in range(_data[n], N):
         if _ans[i] == 0:
             # 왼쪽에 더 큰 숫자들 카운트 맞는지 확인
-            # 0 ~ i-1까지 / n+1 보다 큰 숫자들 카운트 
-            # print(_ans)
-            # if check(i, n+1, tmp):
-            _ans[i] = n+1 # _ans[2] = 1 # -, -, 1, - 
+            _ans[i] = n+1 
             dfs(n+1)
-            _ans[i] = 0
+            _ans[i] = 0 # ERROR : 원상복구 !!!!!!
 
 dfs(0)
