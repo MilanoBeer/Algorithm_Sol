30c090ac998bf7c4486231e52f0006a8783f733c
Hyeyeon Shin <86315623+MilanoBeer@users.noreply.github.com>
2023-07-04 17:29:56 +0900

    [Silver IV] Title: 좋은 단어, Time: 148 ms, Memory: 32824 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -0,0 +1,27 @@
+# 1초 / 256MB
+
+
+N = int(input()) # 단어 수 : 최대 100
+
+ans_cnt = 0
+
+for n in range(N):
+    data = list(input())
+    stack = []
+    for i in data:
+        # 빈 스택에 push
+        if len(stack) == 0:
+            stack.append(i)
+
+        # 아니라면
+        else:
+            # 가장 위에 문자랑, 현재 문자가 같다면
+            if stack[-1] == i:
+                stack.pop()
+            # 다르다면
+            else:
+                stack.append(i)
+    if len(stack) == 0:
+        ans_cnt += 1
+
+print(ans_cnt)
