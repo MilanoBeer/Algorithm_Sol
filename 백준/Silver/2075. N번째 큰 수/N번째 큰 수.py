8dace58a7f2cef6098387f4295bbdbf379a67208
Hyeyeon Shin <86315623+MilanoBeer@users.noreply.github.com>
2023-09-02 17:12:22 +0900

    [Silver II] Title: N번째 큰 수, Time: 992 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -1,7 +1,8 @@
-# 16:10 ~ 16:44 , 메모리 초과 , pypy3 -> 틀림 / 
-# ~ 17:04, gg... / 
-
 import heapq
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
 
 N = int(input())
 hq = []
@@ -14,9 +15,7 @@ for i in range(N):
             heapq.heappush(hq, e)
         else:
             # 현재 최솟값보다 e가 더 작으면 -> 지나가기 : hq에는 최댓값부터 채워져야하므로, 더 작은건 지나간다
-            if e < hq[0]:
-                continue
-            else:
+            if e > hq[0]:
                 tmp = heapq.heappop(hq)
                 heapq.heappush(hq, e)
 print(heapq.heappop(hq))
