49efd80dd0b255b7367d621a361685d75083ee0c
Beer <86315623+MilanoBeer@users.noreply.github.com>
2024-06-29 22:37:54 +0900

    [Gold V] Title: 도로의 개수, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1577. 도로의 개수/도로의 개수.py
+++ 백준/Gold/1577. 도로의 개수/도로의 개수.py
@@ -15,7 +15,17 @@ for k in range(K):
     _not.append((c, d, a, b))
 
 # init
-dp[0][0] = 1
+for i in range(1, N+1):
+    if (0, i-1, 0, i) not in _not:
+        dp[0][i] = 1
+    else:
+        break
+
+for i in range(1, M+1):
+    if (i-1, 0, i, 0) not in _not:
+        dp[i][0] = 1
+    else:
+        break
 # if (0, 0, 0, 1) not in _not:
 #     dp[0][1:] = [1] * (N+1)
 # if (0, 0, 1, 0) not in _not:
@@ -23,15 +33,18 @@ dp[0][0] = 1
 #         dp[i][0] = 1
 
 # travel
-for i in range(M+1):
-    for j in range(N+1):
-        if j > 0:
-            if (i, j-1, i, j) not in _not:
-                dp[i][j] += dp[i][j - 1]
-            # else:
-            #     dp[i][j] = 0
-        if i > 0:
-            if (i-1, j, i, j) not in _not:
-                dp[i][j] += dp[i-1][j]
-        
+for i in range(1, M+1):
+    for j in range(1, N+1):
+        # if j == 0:
+        #     if (i-1, j, i, j) not in _not:
+        #         dp[i][j] += dp[i-1][j]
+        #     # else:
+        #     #     dp[i][j] = 0
+        # else:
+        if (i-1, j, i, j) not in _not:
+            dp[i][j] += dp[i-1][j]
+        if (i, j-1, i, j) not in _not:
+            dp[i][j] += dp[i][j-1]
+# for lst in dp:
+#     print(lst)    
 print(dp[M][N])
