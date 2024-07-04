ec1fdd9b21ceec1a4553c6c3c7d8c933567e0a32
Beer <86315623+MilanoBeer@users.noreply.github.com>
2024-07-04 22:47:59 +0900

    [Silver II] Title: DNA 비밀번호, Time: 504 ms, Memory: 39804 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -5,24 +5,23 @@ A, C, G, T = map(int, input().split())
 cnt_dd = {'A':A, 'C':C, 'G':G, 'T':T}
 ans = 0
 
+def check_dd(dd, cnt_dd):
+    global ans 
+
+    flag = True
+    for k, v in dd.items():
+        if v < cnt_dd[k]:
+            flag = False
+    if flag:
+        ans += 1
+
 # 초기 데이터 저장
 dd = {'A':0, 'C':0, 'G':0, 'T':0}
 for i in range(P):
     dd[_data[i]] += 1
 s, e = 0, P - 1
+check_dd(dd, cnt_dd)
 
-flag = True
-for k, v in dd.items():
-    if v < cnt_dd[k]:
-        flag = False
-if flag:
-    ans += 1    
-
-# while e
-# 기존 s가 가리키고 있던 cnt - 1
-# s증가만
-# e증가
-# e가 새로 가리키는 값 cnt + 1
 while e < S - 1:
     dd[_data[s]] -=  1
     s += 1
@@ -30,12 +29,6 @@ while e < S - 1:
     e += 1
     dd[_data[e]] += 1
 
-    # 현재 dicionary 확인
-    flag = True
-    for k, v in dd.items():
-        if v < cnt_dd[k]:
-            flag = False
-    if flag:
-        ans += 1
+    check_dd(dd, cnt_dd)     # 현재 dicionary 확인
 
 print(ans)
